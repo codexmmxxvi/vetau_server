17965ffa52f0b36ef79fdcf7ad89a2603e28e1c3
Quoc Anh <quocanhlove199@gmail.com>
2026-04-15 13:29:46 +0700

    fix(api-gateway): use WebFlux-native CORS filter

--- api-gateway/src/main/java/codex/mmxxvi/config/CorsConfig.java
+++ api-gateway/src/main/java/codex/mmxxvi/config/CorsConfig.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
@@ -20,6 +20,6 @@ public class CorsConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(source);
+        return new CorsWebFilter(source);
     }
 }
