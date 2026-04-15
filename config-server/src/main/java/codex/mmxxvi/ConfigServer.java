69390527176dcdddd72cb45861a4d17a69c77c99
Quoc Anh <quocanhlove199@gmail.com>
2026-04-15 14:11:55 +0700

    restore(config-server): recover config server module from stash

--- config-server/src/main/java/codex/mmxxvi/ConfigServer.java
+++ config-server/src/main/java/codex/mmxxvi/ConfigServer.java
@@ -2,11 +2,13 @@ package codex.mmxxvi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class Main {
+@EnableConfigServer
+public class ConfigServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(ConfigServer.class, args);
     }
 }
